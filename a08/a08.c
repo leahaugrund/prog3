98a44cbb93edac14173c0f666d5884a24830bf78
leahaugrund <lea@haugrund.de>
2017-11-21 12:20:18 +0100

    einlesen funktioniert

--- a08/a08.c
+++ a08/a08.c
@@ -2,33 +2,31 @@
 
 int main(void){
 	
-	int i = 0, j, maxIndex = 0, maxZahl = 0, aktZahl,alteZahl;
+	int i = 0, j = 0, k = 0, minIndex = 0, minZahl = 0, aktZahl,alteZahl;
 	int zahlen[21];
 	
 	while( scanf("%d", &zahlen[i]) != EOF){
 		i++;
 	}
 	
-		
-	for( j = 0; j <20; j++){
-		for( i = j; i < 20; i++){
-			aktZahl = zahlen[i];
-			if( aktZahl > maxZahl){
-				maxZahl = aktZahl;
-				maxIndex = i;
-			}
+	
+	
+	minZahl = zahlen[k];
+	for( k = 0; k < i; k++){
+		aktZahl = zahlen[k];
+		if( aktZahl < minZahl ){
+			minZahl = aktZahl;
+			minIndex = k;
 		}
-		alteZahl = zahlen[j];
-		zahlen[j]= maxZahl;	
-		zahlen[maxIndex] = alteZahl;
 	}
-	
-	for( i = 0; i < 20; i ++) {
-			printf("%d\n", &zahlen[i]); 
+	alteZahl = zahlen[0];
+	zahlen[minIndex] = alteZahl;
+	zahlen[0] = minZahl;
+
+		
+	for( j = 0; j < i; j ++) {
+		printf("%d\n", zahlen[j]); 
 	}
-	
-	
-	
 	
 	return 0;
 }
